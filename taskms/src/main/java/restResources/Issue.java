5acfc8476d0a794deb1f3d9e9d459b9551a900b0
Nguyen Hai Dang <nguyen.h.dang.1001@gmail.com>
2017-02-18 23:06:20 +0200

    Issue resouces draft

--- taskms/src/main/java/restResources/Issue.java
+++ taskms/src/main/java/restResources/Issue.java
@@ -1,9 +1,36 @@
 package restResources;
 
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
 /**
  * Created by rohan on 2/18/17.
  */
 
-
+@Path("/issues")
 public class Issue {
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public void getIssues() {}
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void createNewIssue() {}
+
+    @Path("/{issueId}")
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public void deleteIssue(){}
+
+    @Path("/{issueId}")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public void getIssueById() {}
+
+    @Path("/{taskId}")
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public void updateTask(){}
 }
